4bae45cd72c51864b6a7db543e848aed5f1e244b
JortHub <jortvandriel@hotmail.com>
2017-08-31 18:33:07 +0200

    Changed the mongo test

--- specs/test_mongo.js
+++ specs/test_mongo.js
@@ -3,6 +3,31 @@ jnode.root = "../specs/";
 
 // TEST //
 var db = jnode.database();
-db.connect("mongo").then(function() {
-	db.close();
-});
+db.connect("mongo")
+.then(function() {
+	db.database_create("test")
+	.then(function() {
+		db.table_create("test_table")
+		.then(function() {
+			db.column_add("test1");
+			db.column_add("test2");
+			db.column_add("test3");
+			db.insert({"test1": "A", "test2": "Wonderful", "test3": "Test"})
+			.then(function() {
+				db.modify({"test1": "A"}, {"test3": "Document"})
+				.then(function() {
+					db.find({"test1": "A"})
+					.then(function(result) {
+						console.log(result);
+					})
+					.catch(console.log);
+				})
+				.catch(console.log);
+			})
+			.catch(console.log);
+		})
+		.catch(console.log);
+	})
+	.catch(console.log);
+})
+.catch(console.log);
