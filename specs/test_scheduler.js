ed0b98b08915412ab3ad47146a561acfe73a6fbb
JortHub <jortvandriel@hotmail.com>
2017-09-08 12:04:00 +0200

    Added a scheduler test

---
+++ specs/test_scheduler.js
@@ -0,0 +1,20 @@
+var jnode = require("../index")();
+jnode.root = "../specs/";
+
+// TEST //
+var scheduler = jnode.scheduler();
+var date = {
+	month: 2,
+	day: 3,
+	hour: 2,
+	minute: 1
+};
+scheduler.schedule(date, "once")
+.then(function() {
+	console.log("I'm a scheduler! And I'm alone.");
+});
+scheduler.schedule(date, "multiple")
+.then(function() {
+	console.log("I'm a scheduler! This may not be my first time.");
+});
+scheduler.cancel_all();
